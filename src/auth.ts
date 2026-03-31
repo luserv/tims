e8f7f87495d893f55267b58ab32b857d4a4ad8a6
user <git.random422@passinbox.com>
2026-03-31 00:58:19 -0500

    fixing building bugs

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,7 @@ import { prisma } from "@/lib/prisma"
 import bcrypt from "bcryptjs"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
   session: { strategy: "jwt" },
   pages: {
     signIn: '/login',
